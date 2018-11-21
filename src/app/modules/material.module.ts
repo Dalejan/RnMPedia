2f99d0af3ebb72d9b6f09df7e40548b564fff895
Dalejan <david1804997@gmail.com>
2018-11-20 23:38:44 -0500

    Depura angular material

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,45 +1,21 @@
 import { MatButtonModule } from "@angular/material/button";
 import { NgModule } from "@angular/core";
-import { MatInputModule } from "@angular/material/input";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatCardModule } from "@angular/material/card";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
-import { MatDialogModule } from "@angular/material";
-import { MatSnackBarModule } from "@angular/material";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatGridListModule } from "@angular/material/grid-list";
 @NgModule({
   imports: [
     MatButtonModule,
-    MatInputModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
     MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatListModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    MatToolbarModule,
-    MatGridListModule
+    MatToolbarModule
   ],
 
   exports: [
     MatButtonModule,
-    MatInputModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
     MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatListModule,
-    MatDialogModule,
-    MatSnackBarModule,
-    MatToolbarModule,
-    MatGridListModule
+    MatToolbarModule
   ]
 })
 export class MaterialModule {}
